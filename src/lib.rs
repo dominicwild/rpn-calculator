09eea9cc64ec2d58c25d491b99f7b7b01c82889b
dominicwild <dominicwild@dominicwild.com>
2022-10-03 17:49:37 +0100

    Testing alternate methods

--- src/lib.rs
+++ src/lib.rs
@@ -15,12 +15,17 @@ pub fn evaluate(inputs: &[CalculatorInput]) -> Option<i32> {
     for input in inputs {
         match input {
             Value(number) => stack.push(*number),
-            Add => {
-                let i1 = stack.pop().map(|num| num + stack.pop().unwrap());
-                if let Some(num) = i1 { stack.push(num)};
-                // let option = stack.pop();
-                // stack.push(i1)
-            },
+            _add => {
+                if let Some(num1) = stack.pop() {
+                    if let Some(num2) = stack.pop() {
+                        stack.push(num1 + num2)
+                    }
+                }
+
+                // let i1 = stack.pop().map(|num| num + stack.pop().unwrap());
+                // if let Some(num) = i1 { stack.push(num)};
+
+            }
             // Subtract=> stack.pop(), stack.pop(),
             // Multiply=> stack.pop(), stack.pop(),
             // Divide=> stack.pop(), stack.pop(),
